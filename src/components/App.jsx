9821c0837228d73c1d1e76211ab71dcbc4f8b98a
OlgaRyzha <orizha11@gmail.com>
2023-02-22 20:16:55 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,15 @@
-import { useEffect } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Title } from './Title/Title';
 import { nanoid } from 'nanoid';
-import storage from 'helpers/storage';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import { useDispatch, useSelector } from 'react-redux';
 import { setContacts, setFilter } from 'redux/contactsSlice';
 
-export const CONTACTS = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
-
 export function App() {
   const contacts = useSelector(state => state.contactsData.contacts);
   const filter = useSelector(state => state.contactsData.filter);
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    storage.save('contacts-list', contacts);
-  }, [contacts]);
 
   const addProduct = contact => {
     if (contacts.some(p => p.name === contact.name)) {
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,35 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({
+  contactsData: contactsReducer,
+});
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    contactsData: contactsReducer,
-  },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
